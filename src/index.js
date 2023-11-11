46b57380505b298ae5ef28aa970fe853304933dd
priyansuhub <muniapriyansu@gmail.com>
2023-11-11 16:24:12 +0530

    custom api error message

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+dotenv.config({
+    path: './env'
+})
+
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port :${process.env.PORT}`)
+    })
+}).catch((err)=>{console.log("connection mongo db failed", err)})
+// const express = express()
+// (async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{console.log("Error", error)})
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`app is listening on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.log("ERROR: ", error)
+//         throw error
+//     }
+// })()
---
+++ src/utils/ApiError.js
@@ -0,0 +1,18 @@
+class ApiError extends Error{
+    constructor(statusCode, error=[],message="Something went wrong", stack=""){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = error
+
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export {ApiError}
